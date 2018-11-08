2f438a5b5941d254c719e45ded681492522beaae
xaviguasch <xaviguasch@gmail.com>
2018-11-08 10:30:37 +0100

    destructuring practice part2

--- destructuring.js
+++ destructuring.js
@@ -34,3 +34,26 @@ const { type, amount } = expense
 // console.log(myAmount); // returns '$45 dollar'
 
 
+
+////////////////
+
+const savedFile = {
+    extension: 'jpeg',
+    name: 'repost',
+    size: 1404
+}
+
+// function fileSummary(file) {
+//     return `The file ${file.name}.${file.extension} is of size ${file.size}`
+// }
+
+// console.log(fileSummary(savedFile)); // Returns: The file repost.jpeg is of size 1404
+
+
+//  we can achieve the same with DESTRUCTURING
+
+function fileSummary({name, extension, size}) {
+    return `The file ${name}.${extension} is of size ${size}`
+}
+ 
+console.log(fileSummary(savedFile)); // Returns: The file repost.jpeg is of size 1404
