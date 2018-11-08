42c9f58432c3cb5b6b3553d7280a0f9a9dd6b353
xaviguasch <xaviguasch@gmail.com>
2018-11-08 22:09:18 +0100

    for-of-loops

---
+++ for-of-loop.js
@@ -0,0 +1,15 @@
+const colors = ['red', 'green', 'blue']
+
+for (let color of colors) {
+    console.log(color); // It prints: red green blue (in separate lines)
+}
+
+const numbers = [1, 2, 3, 4]
+
+let total = 0
+
+for (let number of numbers) {
+    total += number
+}
+
+console.log(total); // It prints: 10
